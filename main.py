7f4b99bccf2a86bad71e844b53dc736057165cf1
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-07-17 12:07:27 +0200

    added a sample dataset and some new features. still have to implemet psutil to get hardware stats

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import json
+import numpy as np
+import psutil
 from pprint import pprint
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-import numpy as np
+from timeit import default_timer as timer
 
-with open("../meta/conf.json", 'r') as f:
+
+with open("meta/conf.json", 'r') as f:
     meta = json.load(f)
 
 conf = meta["model"]
+dataset = meta["dataset"]
 arch = conf["architecture"]
 train = conf["train"]
 results = meta["results"]
 
-data = np.random.random((1000, 100))
-labels = np.random.randint(2, size=(1000, 1))
+data = np.genfromtxt("data/" + dataset["id"] + "/train.csv", delimiter=',', usecols=(0, 1, 2, 3, 4, 5, 6, 7))
+labels = np.genfromtxt("data/" + dataset["id"] + "/train.csv", delimiter=',', usecols=(-1))
 
 if conf["type"] == "Sequential":
     model = Sequential()
@@ -21,7 +25,7 @@ if conf["type"] == "Sequential":
         model.add(Dense(
         d,
         activation=a,
-        input_dim=100)) #TODO: First layer should have input size spec
+        input_dim=8)) #TODO: First layer should have input size spec
 
 model.compile(
     optimizer=conf["optimizer"],
@@ -29,15 +33,18 @@ model.compile(
     metrics=conf["metrics"]
 )
 
+start = timer()
+cpu = psutil.cpu_freq()
 model.fit(
     data,
     labels,
     epochs=train["epochs"],
     batch_size=train["batch_size"]
 )
+end = timer()
 
-data = np.random.random((1000, 100))
-labels = np.random.randint(2, size=(1000, 1))
+data = np.genfromtxt("data/" + dataset["id"] + "/test.csv", delimiter=',', usecols=(0, 1, 2, 3, 4, 5, 6, 7)) #TODO this is very ugly temporary fix
+labels = np.genfromtxt("data/" + dataset["id"] + "/test.csv", delimiter=',', usecols=(-1))
 loss_and_metrics = model.evaluate(
     data,
     labels,
@@ -46,8 +53,9 @@ loss_and_metrics = model.evaluate(
 
 for name,value in zip(model.metrics_names, loss_and_metrics):
     results[name] = value
+results["time"] = end-start
 
 pprint(meta)
 
-with open("../meta/conf.json", 'w') as f:
+with open("meta/conf.json", 'w') as f:
     json.dump(meta, f, ensure_ascii=False, indent=2, sort_keys=True)
