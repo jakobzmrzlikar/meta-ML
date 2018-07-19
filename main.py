13da15f3c8740aa9305d00e7d0b9acd2aa80290d
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-07-19 18:28:17 +0200

    first stage working, however the results are not yet as good as expected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+import csv
 import numpy as np
 import psutil
+import keras
 from pprint import pprint
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -16,16 +18,26 @@ arch = conf["architecture"]
 train = conf["train"]
 results = meta["results"]
 
-data = np.genfromtxt("data/" + dataset["id"] + "/train.csv", delimiter=',', usecols=(0, 1, 2, 3, 4, 5, 6, 7))
-labels = np.genfromtxt("data/" + dataset["id"] + "/train.csv", delimiter=',', usecols=(-1))
+
+with open("data/" + dataset["id"] + "/train.csv") as d:
+    reader = csv.reader(d, delimiter=',')
+    data = list(reader)
+    data = np.array(data).astype(float)
+    np.random.shuffle(data)
+
+x = data[:, :-1]
+y = keras.utils.to_categorical(data[:, -1])
+
+dataset["instances"] = data.shape[0]
+dataset["features"] = data.shape[1]-1
+
 
 if conf["type"] == "Sequential":
     model = Sequential()
     for d, a in zip(arch["Dense"], arch["Activation"]):
         model.add(Dense(
         d,
-        activation=a,
-        input_dim=8)) #TODO: First layer should have input size spec
+        activation=a)) #TODO: First layer should have input size spec
 
 model.compile(
     optimizer=conf["optimizer"],
@@ -33,21 +45,29 @@ model.compile(
     metrics=conf["metrics"]
 )
 
+
 start = timer()
 
 model.fit(
-    data,
-    labels,
+    x,
+    y,
     epochs=train["epochs"],
-    batch_size=train["batch_size"]
+    batch_size=train["batch_size"],
+    verbose=1,
 )
 end = timer()
 
-data = np.genfromtxt("data/" + dataset["id"] + "/test.csv", delimiter=',', usecols=(0, 1, 2, 3, 4, 5, 6, 7)) #TODO this is very ugly temporary fix
-labels = np.genfromtxt("data/" + dataset["id"] + "/test.csv", delimiter=',', usecols=(-1))
+with open("data/" + dataset["id"] + "/train.csv") as d:
+    reader = csv.reader(d, delimiter=',')
+    data = list(reader)
+    data = np.array(data).astype(float)
+
+x = data[:, :-1]
+y = keras.utils.to_categorical(data[:, -1])
+
 loss_and_metrics = model.evaluate(
-    data,
-    labels,
+    x,
+    y,
     batch_size=train["batch_size"]
 )
 
