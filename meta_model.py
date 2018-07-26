5f456a575b637420e7cfed06363fc63bbecfac65
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2018-07-26 19:40:26 +0200

    slightly improved the model

--- meta_model.py
+++ meta_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import regularizers
 from sklearn.model_selection import train_test_split
 
 data = np.load("meta/data.npy")
@@ -10,11 +11,12 @@ y = data[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(
     x,
     y,
-    test_size=0.15,
+    test_size=0.2,
     random_state=2)
 
 model = Sequential()
 model.add(Dense(units=64, activation='relu', input_dim=16))
+model.add(Dense(units=32, activation='sigmoid'))
 model.add(Dense(units=32, activation='relu'))
 model.add(Dense(units=16, activation='relu'))
 model.add(Dense(units=1, activation='linear'))
@@ -24,12 +26,10 @@ model.fit(
     x_train,
     y_train,
     epochs=100,
-    batch_size=1,
+    batch_size=16,
     verbose=1,
 )
 loss_and_metrics = model.evaluate(x_test, y_test)
-print()
-print("Test loss:", loss_and_metrics)
 print()
 
 classes = model.predict(x_test)
@@ -38,5 +38,8 @@ for i in range(len(classes)):
     error = int(abs(classes[i][0]-y_test[i]))
     print("Predicted: {}, actual: {}, error: {}".format(
     int(classes[i][0]), y_test[i], error))
+
+print()
+print("Test loss:", loss_and_metrics)
 
 model.save_weights("meta/weights.npy")
