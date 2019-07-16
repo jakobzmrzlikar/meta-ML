12e028d582297650f145cbbf45ba3f18dbd34ec6
Jakob <jakob.zmrzlikar@gmail.com>
2019-07-16 17:38:28 +0200

    some code seperaton to allow for easy additional model types

--- core.py
+++ core.py
@@ -8,13 +8,58 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from timeit import default_timer as timer
 
+
+def build_model(arch, num_features, hyperparams):
+    if hyperparams["type"] == "Sequential":
+        model = Sequential()
+        model.add(Dense(num_features,
+                        activation='linear', input_shape=(num_features,)))
+
+        for d, a in zip(arch["Dense"], arch["Activation"]):
+            model.add(Dense(
+                d,
+                activation=a))
+
+    model.compile(
+        optimizer=hyperparams["optimizer"],
+        loss=hyperparams["loss"],
+        metrics=hyperparams["metrics"]
+    )
+
+    return model
+
+
+def test_model(model, batch_size, datatset, test=None, preprocess_data=True, binary=False):
+    if test is None:
+        with open("data/" + dataset["id"] + "/test.csv", 'r') as d:
+            reader = csv.reader(d, delimiter=',')
+            test = list(reader)
+            test = np.array(test, dtype="float")
+
+    if preprocess_data:
+        np.random.shuffle(test)
+        x = test[:, :-1]
+        if binary:
+            y = test[:, -1]
+        else:
+            y = keras.utils.to_categorical(test[:, -1])
+    else:
+        x = test[0]
+        y = test[1]
+
+    loss_and_metrics = model.evaluate(
+        x,
+        y,
+        batch_size=batch_size
+    )
+    return loss_and_metrics
+
 def run(config, train=None, test=None, preprocess_data=True, binary=False):
     with open(config, 'r') as f:
         meta = json.load(f)
 
     dataset = meta["dataset"]
     conf = meta["model"]
-    arch = conf["architecture"]
     hyperparams = conf["hyperparameters"]
     results = meta["results"]
 
@@ -39,22 +84,7 @@ def run(config, train=None, test=None, preprocess_data=True, binary=False):
     dataset["instances"] = len(x)
     dataset["features"] = len(x[0])
 
-    shape = (dataset["features"],)
-
-    if hyperparams["type"] == "Sequential":
-        model = Sequential()
-        model.add(Dense(dataset["features"], activation='linear', input_shape=shape))
-
-        for d, a in zip(arch["Dense"], arch["Activation"]):
-            model.add(Dense(
-            d,
-            activation=a))
-
-    model.compile(
-        optimizer=hyperparams["optimizer"],
-        loss=hyperparams["loss"],
-        metrics=hyperparams["metrics"]
-    )
+    model = build_model(arch=conf["architecture"], num_features=dataset['features'], hyperparams=hyperparams)
 
     start = timer()
 
@@ -63,38 +93,15 @@ def run(config, train=None, test=None, preprocess_data=True, binary=False):
         y,
         epochs=hyperparams["epochs"],
         batch_size=hyperparams["batch_size"],
-        verbose=1,
+        verbose=0,
     )
     end = timer()
-
-    if test is None:
-        with open("data/" + dataset["id"] + "/test.csv", 'r') as d:
-            reader = csv.reader(d, delimiter=',')
-            test = list(reader)
-            test = np.array(test, dtype="float")
-
-    if preprocess_data:
-        np.random.shuffle(test)
-        x = test[:, :-1]
-        if binary:
-            y = test[:, -1]
-        else:
-            y = keras.utils.to_categorical(test[:, -1])
-    else:
-        x = test[0]
-        y = test[1]
-
-    loss_and_metrics = model.evaluate(
-        x,
-        y,
-        batch_size=hyperparams["batch_size"]
-    )
-
-    for name,value in zip(model.metrics_names, loss_and_metrics):
-        results[name] = value
     results["time"] = end-start
 
-    pprint(meta)
+    loss_and_metrics = test_model(model, hyperparams["batch_size"], dataset, test, preprocess_data, binary)
+    for name,value in zip(model.metrics_names, loss_and_metrics):
+        results[name] = value  
 
+    pprint(meta)
     with open(config, 'w') as f:
         json.dump(meta, f, ensure_ascii=False, indent=2, sort_keys=True)
