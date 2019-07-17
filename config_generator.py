5cfc1cb902018398413cdab2da4f48597f73e4dd
Jakob <jakob.zmrzlikar@gmail.com>
2019-07-17 13:41:05 +0200

    seperated build and fit hyperparameters for easier integration of other types of models

--- config_generator.py
+++ config_generator.py
@@ -7,6 +7,8 @@ def generate(dataset):
         encoding = json.load(f)
 
     hyperparams = meta["model"]["hyperparameters"]
+    build = hyperparams["build"]
+    fit = hyperparams["fit"]
     arch = meta["model"]["architecture"]
     epch = [10, 20, 50, 100]
     acti = ["relu", "sigmoid", "hard_sigmoid", "tanh"]
@@ -15,13 +17,13 @@ def generate(dataset):
     for m in range(4):
         arch["Activation"][0] = acti[m]
         for n in encoding["optimizer"]:
-            hyperparams["optimizer"] = n
+            build["optimizer"] = n
             for k in range(4):
                 arch["Dense"][0] = 2**(k+5)
                 for i in range(4):
-                    hyperparams["epochs"] = epch[i]
+                    fit["epochs"] = epch[i]
                     for j in range(4):
-                        hyperparams["batch_size"] = 2**(j+4)
+                        fit["batch_size"] = 2**(j+4)
                         idx+=1
                         name = "config/generated/"+dataset+'_'+str(idx)+".json"
                         with open(name, 'w') as f:
